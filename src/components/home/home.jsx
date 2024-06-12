8e5cbdd5e213fc17c094ab4df37b5c8de3ff35ac
ShaileshGaikwad801 <2020bec049@sggs.ac.in>
2024-06-12 23:23:05 +1000

    initial

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import CarouselDarkVariant from "./Carousel";
-import SearchBar from "./SearchBar";
-import SearchResults from "./SearchResults";
+
 import Services from "./services";
 import Testimonials from "./testimonials";
 import Contact from "./ContactUs";
@@ -10,26 +9,13 @@ import Navbar from "../Navbar/Navbar";
 import Mycards from "./Mycards";
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedState, setSelectedState] = useState("");
+
 
   return (
     <div className="min-h-screen flex flex-col">
       
       <CarouselDarkVariant />
        <Navbar/>
-      {/* <div className="flex-grow">
-        <div className="container mx-auto px-8 py-8">
-          <h1 className="text-3xl font-bold mb-4">Top Places in India</h1>
-          <SearchBar 
-            searchTerm={searchTerm} 
-            setSearchTerm={setSearchTerm} 
-            selectedState={selectedState} 
-            setSelectedState={setSelectedState} 
-          />
-          <SearchResults searchTerm={searchTerm} selectedState={selectedState} />
-        </div>
-      </div> */}
       <Mycards/>
       <Services />
       <Testimonials />
